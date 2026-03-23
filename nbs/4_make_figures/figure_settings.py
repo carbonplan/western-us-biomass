6222092e30ce717eb20829f900e5678905fdfe12
Claire Zarakas <czarakas@uw.edu>
2026-03-23 09:39:09 -0700

    Update figure settings with colors

--- nbs/4_make_figures/figure_settings.py
+++ nbs/4_make_figures/figure_settings.py
@@ -1,5 +1,24 @@
-# plot_style.py
 import matplotlib as mpl
+
+panel_x_offset = 0.02
+panel_y_offset = 0.98
+
+# Figure 1 colors
+color_MTBS = "#A24936"
+color_MTBS_light = "#FBBBA8"
+color_FIA_fire = "#E55713"
+
+# Figure 3 colors
+color_unburned = "#64b9c4"
+color_net = "#282B28"
+color_burned = "#f07071"
+
+# Figure 5 colors
+color_CMIP = "#A24936"
+color_Liu = "#83BCA9"
+color_this_study = "#282B28"
+color_Li = "#3E5641"
+color_EPA = "#D36135"
 
 style_settings = {
     # Font
@@ -8,7 +27,7 @@ style_settings = {
         "Helvetica",
         "Arial",
         "DejaVu Sans",
-    ],  # Arial as fallback if Helvetica not installed
+    ],
     "font.size": 24,
     "axes.titlesize": 26,
     "axes.labelsize": 24,
@@ -20,9 +39,12 @@ style_settings = {
     "axes.spines.right": True,
     "xtick.direction": "in",
     "ytick.direction": "in",
+    "xtick.major.pad": 5.5,
+    "ytick.major.pad": 5.5,
     "xtick.top": True,
     "ytick.right": True,
-    "axes.labelpad": 10,
+    "axes.labelpad": 8,
+    "axes.titlepad": 10,
     # Lines
     "lines.linewidth": 8,
     # Layout
